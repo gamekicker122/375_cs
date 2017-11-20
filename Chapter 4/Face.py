3bf756a696af766922d00c0e400d4e0544635026
gamekicker122 <byoung21@hanoverstudents.org>
2017-11-20 11:32:29 -0500

    face project 11/20/17

--- Chapter 4/Face.py
+++ Chapter 4/Face.py
@@ -5,21 +5,69 @@ def main():
     win = GraphWin("Face Drawing", 400, 400)
 
     # Here's where I will draw the face!
-    center = Point (100, 100)
-    circ = Circle(center, 30)
-    circ.setFill('red')
+    center = Point (195 , 172)
+    circ = Circle(center, 175)
+    circ.setFill('orange')
     circ.draw(win)
+
+
+
+    center = Point (100, 95)
+    circ = Circle(center, 30)
+    circ.setFill('cyan')
+    circ.draw(win)
+
+
+    center = Point (100, 95)
+    circ = Circle(center, 15)
+    circ.setFill('black')
+    circ.draw(win)
+
 
     # this is the right eye
-    center = Point (300, 100)
+    center = Point (300, 95)
     circ = Circle(center, 30)
-    circ.setFill('red')
+    circ.setFill('cyan')
     circ.draw(win)
 
 
-    rectangle = Rectangle (Point(50,210) , Point(350 , 250))
+    center = Point (300, 95)
+    circ = Circle(center, 15)
+    circ.setFill('black')
+    circ.draw(win)
+
+    rectangle = Rectangle (Point(50,240) , Point(350 , 245))
     rectangle.draw(win)
-    rectangle.setFill('red')
+    rectangle.setFill('cyan')
+
+
+    line = Line (Point(255,300) , Point(155 , 300))
+    line.draw(win)
+    line.setFill('cyan')
+
+
+    line = Line (Point(50,245) , Point(155,300))
+    line.draw(win)
+    line.setFill('cyan')
+
+
+    line = Line (Point(350,245) , Point(255,300))
+    line.draw(win)
+    line.setFill('cyan')
+
+    line = Line (Point(150,40) , Point (100,40))
+    line.draw(win)
+    line.setFill('black')
+
+    line = Line (Point(250,40) , Point (300,40))
+    line.draw(win)
+    line.setFill('black')
+
+    polygon = Polygon (Point(225,180) , Point(150, 180) , Point(185, 140))
+    polygon.draw(win)
+    polygon.setFill('blue')
+
+
     input("press any key to quit")
 
 
