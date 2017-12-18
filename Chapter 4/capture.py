05dc42fd47596ef224a5bca7d50b72d67bc178d6
gamekicker122 <byoung21@hanoverstudents.org>
2017-12-18 11:34:01 -0500

    Capture 12/14/17

--- Chapter 4/capture.py
+++ Chapter 4/capture.py
@@ -1,71 +1,73 @@
-# animate_with_keys.py
+# capture_the_pellets.py
 #
-# This program illustrates how to animate a drawing using input from the keyboard.
+# This program is a simple game called "Capture the Pellets!". The user moves a large white ball around the screen
+# using the arrow keys to "capture" a small, randomly-placed, yellow pellet. The program keeps track of the score and a
+# countdown timer. The goal is to capture as many pellets as possible before time expires.
 #
 # by Mr. Ciccolo
 
-
-
-
 from graphics import *
-from random import *
 from math import *
+from random import *
+
 
 def main():
-    print("Press the arrow keys to move the white circle, press 'q' to quit")
+    print("Press the arrow keys to move the white ball and capture the yellow pellets. Press 'q' to quit.")
 
-    score=0
-    timer=60
-    power_pellet=5
-    power_time_check=5
-    power_time_pellet=0
-    power_timer=5
+    score = 0
+    timer = 60
+    power_pellet = 5
+    double_points = 5
 
     window_size = 600
     canvas_size = 20
 
-    win = GraphWin("Animation Example with Keyboard", window_size, window_size)
+    win = GraphWin("Capture the Pellets!", window_size, window_size)
     win.setCoords(0, 0, canvas_size, canvas_size)
     win.setBackground("black")
 
-
-    score_text = Text(Point(2,18),"Score:0")
+    score_text = Text(Point(0.2 * canvas_size, 0.9 * canvas_size), "Score: " + str(score))
     score_text.setTextColor("white")
     score_text.setFace("courier")
     score_text.setSize(24)
     score_text.draw(win)
 
+    timer_text = Text(Point(0.8 * canvas_size, 0.9 * canvas_size), "Time: " + str(timer))
+    timer_text.setTextColor("white")
+    timer_text.setFace("courier")
+    timer_text.setSize(24)
+    timer_text.draw(win)
 
-    time_text = Text(Point(15,18),"Timer:60")
-    time_text.setTextColor("white")
-    time_text.setFace("courier")
-    time_text.setSize(24)
-    time_text.draw(win)
-
-    circle = Circle(Point(canvas_size / 2, canvas_size / 2), 2)
-    circle.setFill("white")
-    circle.draw(win)
-
-    pellet = Circle(Point(0,0),1)
-    pellet.setFill("yellow")
-    pellet.draw(win)
-    move_to_random_point(pellet,canvas_size)
-
-    power_pellet = Circle(Point(0,0),1)
-    power_pellet.setFill("green")
-    power_pellet.draw(win)
-    move_to_random_point(power_pellet,canvas_size)
-
-    message_text = Text(Point(canvas_size/2, canvas_size/2), "CLICK TO START")
-    message_text.setFill("green")
+    message_text = Text(Point(canvas_size / 2, canvas_size / 2), "CLICK TO START")
+    message_text.setTextColor("green")
     message_text.setFace("courier")
-    message_text.setSize(35)
+    message_text.setSize(36)
     message_text.draw(win)
+
     win.getMouse()
     message_text.setText("")
 
-    time_check=time.time()
+    ball = Circle(Point(canvas_size / 2, canvas_size / 2), 2)
+    ball.setFill("white")
+    ball.draw(win)
 
+    pellet = Circle(Point(0, 0), 1)
+    pellet.setFill("yellow")
+    pellet.draw(win)
+    move_to_random_point(pellet, canvas_size)
+
+    power_pellet = Circle(Point(-10,-10),1)
+    power_pellet.setFill("green")
+    power_pellet.draw(win)
+
+
+    double_points
+    hidden = true
+
+
+
+
+    time_check = time.time()
     while timer > 0:
         key = win.checkKey()
 
@@ -75,55 +77,67 @@ def main():
         if key == "q":
             break
         elif key == "Up":
-            delta_y += 1
+            delta_y = 1
         elif key == "Down":
-            delta_y -= 1
+            delta_y = -1
         elif key == "Left":
-            delta_x -= 1
+            delta_x = -1
         elif key == "Right":
-            delta_x += 1
+            delta_x = 1
 
-        circle.move(delta_x, delta_y)
+        ball.move(delta_x, delta_y)
 
-        if collide(circle,pellet):
-            move_to_random_point(pellet,  canvas_size)
-            score +=1
-            score_text.setText("score:"+str(score))
+        if collide(ball, pellet):
+            score += 1
+            score_text.setText("Score: " + str(score))
+            move_to_random_point(pellet, canvas_size)
 
-        if collide(circle,power_pellet):
-            move_to_random_point(power_pellet,  canvas_size)
-            score +=5
-            score_text.setText("score:"+str(score))
-
-        if time.time() - time_check > 1:
-            timer -=1
-            print(timer)
+        if (time.time() - time_check) > 1:
             time_check = time.time()
-            time_text.setText("Timer:"+str(timer))
-
-        if power_time_check() - time_check > 2:
-            power_timer-=1
-            print("Timer 5 seconds")
-            power_time_check = time.time()
+            timer -= 1
+            timer_text.setText("Time: " + str(timer))
 
 
-def collide(c1,c2):
+        if collide(ball, power_pellet):
+            score += 5
+            score_text.setText("Score: " + str(score))
+            move_to_random_point(power_pellet, canvas_size)
+
+        if timer % 5 ==0:
+           move_to_random_point(power_pellet, canvas_size)
+
+
+
+    message_text.setTextColor("red")
+    message_text.setText("GAME OVER")
+
+    input("Game over! Your final score was " + str(score) + ". Press Enter to quit.")
+
+
+def collide(c1, c2):
+    # Calculate the distance between the center points of the circles using Pythagorean theorem (a^2 + b^2 = c^2)
     a = c1.getCenter().getX() - c2.getCenter().getX()
     b = c1.getCenter().getY() - c2.getCenter().getY()
-    c = sqrt (a**2 + b**2)
-    return c<= (c1.getRadius() +c2.getRadius())
+    distance = sqrt(a**2 + b**2)
+
+    collision = distance <= (c1.getRadius() + c2.getRadius())
+
+    return collision
 
 
-def move_to_random_point(circle,bounds):
-    next_x= randrange(circle.getRadius(),bounds+1 - circle.getRadius())
-    next_y= randrange(circle.getRadius(),bounds+1 - circle.getRadius())
-
+def move_to_random_point(circle, bounds):
     current_x = circle.getCenter().getX()
     current_y = circle.getCenter().getY()
 
-    delta_x=next_x - current_x
-    delta_y=next_y - current_y
+    next_x = randrange(circle.getRadius(), bounds + 1 - circle.getRadius())
+    next_y = randrange(circle.getRadius(), bounds + 1 - circle.getRadius())
 
-    circle.move(delta_x,delta_y)
+    delta_x = next_x - current_x
+    delta_y = next_y - current_y
+
+    circle.move(delta_x, delta_y)
+
+
+
 
 main()
